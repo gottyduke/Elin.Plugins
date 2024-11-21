ffa044389b559d090f15f22c348dfe5f57409eac
DK <35783662+gottyduke@users.noreply.github.com>
2024-11-21 08:08:32 -0600

    fix(VSS): body tex downscaling

--- VariableSpriteSupport/Patches/BuildSpritesPatch.cs
+++ VariableSpriteSupport/Patches/BuildSpritesPatch.cs
@@ -41,9 +41,9 @@ internal class BuildSpritesPatch
         VssMod.Log("rebuilding sprites...");
 
         var pccm = PCC.pccm;
-        var body = pcc.layerList.list[pcc.layerList.indexBody].tex;
+        var body = pcc.layerList.list[pcc.layerList.indexBody].tex as Texture2D;
 
-        var maxWidth = body.width;
+        var maxWidth = body!.width;
         var maxHeight = body.height;
         // enforce 2:3 ratio for body tex
         if (maxHeight / maxWidth != 48 / 32) {
@@ -51,82 +51,104 @@ internal class BuildSpritesPatch
             return false;
         }
 
-        // downscale body tex
-        const int bodyWidth = 32 * 4;
-        const int bodyHeight = 48 * 4;
-        if (maxWidth > bodyWidth && maxHeight > bodyHeight) {
-            VssMod.Log($"body tex is larger than {bodyWidth}x{bodyHeight}, downscaling from {maxWidth}x{maxHeight}");
-            var bodyRt = new RenderTexture(bodyWidth, bodyHeight, 0, RenderTextureFormat.ARGB32);
-            RenderTexture.active = bodyRt;
+        try {
+            // downscale body tex
+            const int bodyWidth = 32 * 4;
+            const int bodyHeight = 48 * 4;
+            if (maxWidth > bodyWidth && maxHeight > bodyHeight) {
+                VssMod.Log(
+                    $"body tex is larger than {bodyWidth}x{bodyHeight}, downscaling from {maxWidth}x{maxHeight}");
+
+                var bodyRt = new RenderTexture(bodyWidth, bodyHeight, 0, RenderTextureFormat.ARGB32);
+
+                RenderTexture.active = bodyRt;
+                GL.Clear(true, true, Color.clear);
+
+                pccm.mat.SetColor(_color, pcc.layerList.list[pcc.layerList.indexBody].color);
+                Graphics.Blit(body, bodyRt, pccm.mat);
+
+                body = new(bodyWidth, bodyHeight, TextureFormat.ARGB32, false);
+                Graphics.CopyTexture(bodyRt, body);
+
+                maxWidth = bodyWidth;
+                maxHeight = bodyHeight;
+            }
+
+            // iterate all layers to get max
+            for (var i = pcc.layerList.list.Count - 1; i >= 0; --i) {
+                var tex = pcc.layerList.list[i].tex;
+                if (i == pcc.layerList.indexBody) {
+                    tex = body;
+                }
+
+                maxWidth = Math.Max(tex.width, maxWidth);
+                maxHeight = Math.Max(tex.height, maxHeight);
+            }
+
+            VssMod.Log($"max tex {maxWidth}x{maxHeight}");
+
+            var rebuildTexSheet = false;
+            var renderTexture = new RenderTexture(maxWidth, maxHeight, 0, RenderTextureFormat.ARGB32);
+
+            RenderTexture.active = renderTexture;
             GL.Clear(true, true, Color.clear);
 
-            body = new Texture2D(bodyWidth, bodyHeight, TextureFormat.ARGB32, false);
-            pccm.mat.SetColor(_color, pcc.layerList.list[pcc.layerList.indexBody].color);
-            Graphics.Blit(body, bodyRt, pccm.mat);
-            Graphics.CopyTexture(bodyRt, body);
-        }
+            for (var i = pcc.layerList.list.Count - 1; i >= 0; --i) {
+                var layer = pcc.layerList.list[i];
+                pccm.mat.SetColor(_color, layer.color);
 
-        // iterate all layers to get max
-        foreach (var layer in pcc.layerList.list) {
-            maxWidth = Math.Max(layer.tex.width, maxWidth);
-            maxHeight = Math.Max(layer.tex.height, maxHeight);
-        }
+                var tex = i != pcc.layerList.indexBody
+                    ? layer.tex as Texture2D
+                    : body;
+                // calculate layer offset
+                var offsetWidth = maxWidth - tex!.width;
+                var offsetHeight = maxHeight - tex.height;
 
-        VssMod.Log($"max tex {maxWidth} x {maxHeight}");
+                VssMod.Log($"rebuilding layer {i}, tex {tex.width}x{tex.height}, offset {offsetWidth}x{offsetHeight}");
 
-        var rebuildTexSheet = false;
-        var renderTexture = new RenderTexture(maxWidth, maxHeight, 0, RenderTextureFormat.ARGB32);
-        RenderTexture.active = renderTexture;
-        GL.Clear(true, true, Color.clear);
+                // extend layer tex if necessary
+                var extend = offsetWidth != 0 || offsetHeight != 0;
+                if (extend) {
+                    VssMod.Log(">> extending layer...");
+                    tex = tex.ExtendBlit(maxWidth, maxHeight);
+                    rebuildTexSheet = true;
+                }
 
-        for (var i = pcc.layerList.list.Count - 1; i >= 0; --i) {
-            var layer = pcc.layerList.list[i];
-            pccm.mat.SetColor(_color, layer.color);
-            // calculate layer offset
-            var offsetWidth = maxWidth - layer.tex.width;
-            var offsetHeight = maxHeight - layer.tex.height;
-
-            VssMod.Log(
-                $"rebuilding layer {i}, width {layer.tex.width} height {layer.tex.height}, offset width {offsetWidth}, offset height {offsetHeight}");
-
-            // extend layer tex if necessary
-            var extend = offsetWidth != 0 || offsetHeight != 0;
-            var tex = layer.tex as Texture2D;
-            if (extend) {
-                VssMod.Log(">> extending layer...");
-                tex = tex!.ExtendBlit(maxWidth, maxHeight);
-                rebuildTexSheet = true;
+                Graphics.Blit(tex, renderTexture, pccm.mat);
             }
 
-            Graphics.Blit(tex, renderTexture, pccm.mat);
-        }
+            var needRebuild = false;
+            var variation = pcc.variation;
 
-        var needRebuild = false;
-        var variation = pcc.variation;
+            if (!variation.tex || rebuildTexSheet ||
+                variation.tex.width != renderTexture.width ||
+                variation.tex.height != renderTexture.height) {
+                if (variation.tex) {
+                    Object.DestroyImmediate(variation.tex);
+                    needRebuild = true;
+                }
 
-        if (!variation.tex || rebuildTexSheet ||
-            variation.tex.width != renderTexture.width ||
-            variation.tex.height != renderTexture.height) {
-            if (variation.tex) {
-                Object.DestroyImmediate(variation.tex);
-                needRebuild = true;
+                var data = TextureImportSetting.Instance
+                    ? TextureImportSetting.Instance.data
+                    : IO.importSetting;
+                variation.tex = new(renderTexture.width, renderTexture.height, data.format, data.mipmap, data.linear) {
+                    wrapMode = data.wrapMode,
+                    filterMode = data.filterMode,
+                };
             }
 
-            var data = TextureImportSetting.Instance
-                ? TextureImportSetting.Instance.data
-                : IO.importSetting;
-            variation.tex = new(renderTexture.width, renderTexture.height, data.format, data.mipmap, data.linear) {
-                wrapMode = data.wrapMode,
-                filterMode = data.filterMode,
-            };
+            Graphics.CopyTexture(renderTexture, variation.tex);
+            if (variation.main is null || needRebuild) {
+                variation.BuildSprites(variation.tex);
+            }
+
+            VssMod.Log("finished rebuilding sprites");
+        } catch (Exception ex) {
+            VssMod.Log("failed rebuilding sprites");
+            VssMod.Log(ex);
+            return false;
         }
 
-        Graphics.CopyTexture(renderTexture, variation.tex);
-        if (variation.main is null || needRebuild) {
-            variation.BuildSprites(variation.tex);
-        }
-
-        VssMod.Log("finished rebuilding sprites...");
         return true;
     }
 }
