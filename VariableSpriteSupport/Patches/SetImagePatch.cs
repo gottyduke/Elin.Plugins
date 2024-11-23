dadf42bccdb0df980c7ca75a1bfa8cb4fdebb7db
DK <35783662+gottyduke@users.noreply.github.com>
2024-11-23 08:12:40 -0600

    feat(VSS): update sprite patch

---
+++ VariableSpriteSupport/Patches/SetImagePatch.cs
@@ -0,0 +1,22 @@
+﻿using HarmonyLib;
+
+namespace VSS.Patches;
+
+[HarmonyPatch]
+internal class SetImagePatch
+{
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(ItemGeneral), nameof(ItemGeneral.SetChara), typeof(Chara))]
+    internal static void OnSetImage(ItemGeneral __instance, Chara c)
+    {
+        if (!c.IsPCC) {
+            return;
+        }
+
+        var sprite = __instance.button1.icon.sprite;
+        var rect = __instance.button1.icon.rectTransform;
+
+        var offsetHeight = sprite.textureRect.height - 48;
+        rect.position = rect.position with { y = rect.position.y + offsetHeight };
+    }
+}
