27df0ed24271d7cd57fdd8d86f46052f723d02e5
DK <35783662+gottyduke@users.noreply.github.com>
2024-12-04 13:18:57 -0600

    i18n(EC): add `ZHTW` & `KR` loc

--- EquipmentComparison/EquipmentComparisonLoc.cs
+++ EquipmentComparison/EquipmentComparisonLoc.cs
@@ -5,12 +5,16 @@ internal static class EquipmentComparisonLoc
     internal static string EquippedIndicator => Lang.langCode switch {
         "CN" => "(已装备)",
         "JP" => "(装備中)",
+        "ZHTW" => "(已裝備)",
+        "KR" => "(장착 완료)",
         _ => "(Equipped)",
     };
 
     internal static string CarriedIndicator => Lang.langCode switch {
         "CN" => "(已携带)",
-        "JP" => "(装備中)", // ? 持っている??
+        "JP" => "(所持中)",
+        "ZHTW" => "(已携帶)",
+        "KR" => "(소지 중)",
         _ => "(Carried)",
     };
 
@@ -19,6 +23,8 @@ internal static class EquipmentComparisonLoc
         return Lang.langCode switch {
             "CN" => "装备比较: " + (on ? "启用 " : "禁用 "),
             "JP" => "装備比較: " + (on ? "启用 " : "無効 "),
+            "ZHTW" => "裝備比較: " + (on ? "啓用 " : "禁用 "),
+            "KR" => "장비 비교: " + (on ? "사용 중 " : "사용 안 함 "),
             _ => "Compare Equipment: " + (on ? "Enabled " : "Disabled "),
         };
     }
