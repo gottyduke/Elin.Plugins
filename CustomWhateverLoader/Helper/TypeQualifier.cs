727b2538bb90e215e34f0aebeeca7cf3d7efa251
DK <35783662+gottyduke@users.noreply.github.com>
2024-12-18 22:41:36 -0600

    feat(CWL): add `TypeQualifier`

---
+++ CustomWhateverLoader/Helper/TypeQualifier.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using BepInEx;
+using UnityEngine;
+
+namespace Cwl.Helper;
+
+public class TypeQualifier
+{
+    private static List<TypeInfo>? _declared;
+
+    public static Type? TryQualify(string unqualified, string assemblyOverride = "")
+    {
+        _declared ??= Resources.FindObjectsOfTypeAll<BaseUnityPlugin>()
+            .SelectMany(p => p.GetType().Assembly.DefinedTypes)
+            .Where(t => typeof(Act).IsAssignableFrom(t))
+            .ToList();
+
+        var qualified = _declared.FirstOrDefault(t => t.FullName == unqualified) ??
+                        _declared.FirstOrDefault(t => t.Name == unqualified);
+        if (qualified?.FullName is null ||
+            (assemblyOverride is not "" && qualified.Assembly.GetName().Name != assemblyOverride)) {
+            return null;
+        }
+
+        return qualified;
+    }
+}
