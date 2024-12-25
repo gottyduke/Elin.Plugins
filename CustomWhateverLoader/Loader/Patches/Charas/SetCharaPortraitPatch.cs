89b3d136ecf173aa4177bfb749da1e99b3c92499
DK <35783662+gottyduke@users.noreply.github.com>
2024-12-25 11:01:32 -0600

    fix(CWL): do not cache chara portrait repositioner

--- CustomWhateverLoader/Loader/Patches/Charas/SetCharaPortraitPatch.cs
+++ CustomWhateverLoader/Loader/Patches/Charas/SetCharaPortraitPatch.cs
@@ -1,9 +1,7 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using Cwl.API.Custom;
 using Cwl.Helper.Unity;
 using HarmonyLib;
-using UnityEngine;
 
 namespace Cwl.Loader.Patches.Charas;
 
@@ -12,7 +10,6 @@ internal class SetCharaPortraitPatch
 {
     // calculated from all game npc average
     private const float AverageDistance = 54.5f;
-    private static readonly Dictionary<string, Vector3> _cached = [];
 
     [HarmonyPostfix]
     [HarmonyPatch(typeof(Portrait), nameof(Portrait.SetChara))]
@@ -31,24 +28,18 @@ internal class SetCharaPortraitPatch
             return;
         }
 
-        if (!_cached.TryGetValue(c.source.id, out var cached)) {
-            var sprite = __instance.imageChara.sprite;
+        var sprite = __instance.imageChara.sprite;
 
-            var dist = sprite.NearestPerceivableMulticast(4, 4,
-                (int)sprite.rect.width / 2,
-                (int)sprite.rect.height,
-                directionY: -1);
+        var dist = sprite.NearestPerceivableMulticast(4, 4,
+            (int)sprite.rect.width / 2,
+            (int)sprite.rect.height,
+            directionY: -1);
 
-            var pos = __instance.imageChara.transform.localPosition;
-            if (dist > AverageDistance) {
-                _cached[c.source.id] = pos;
-                return;
-            }
-
-            cached = pos with { y = pos.y - (AverageDistance - dist) / 2 };
-            _cached[c.source.id] = cached;
+        var pos = __instance.imageChara.transform.localPosition;
+        if (dist > AverageDistance) {
+            return;
         }
 
-        __instance.imageChara.transform.localPosition = cached;
+        __instance.imageChara.transform.localPosition = pos with { y = pos.y - (AverageDistance - dist) / 2 };
     }
 }
