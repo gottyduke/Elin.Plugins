24a7021fc761a387db9f12a718690cf3fe5572cd
DK <35783662+gottyduke@users.noreply.github.com>
2025-01-08 22:53:58 -0600

    feat: CWL Example: Sparkly Water

---
+++ CwlExamples/SparklyWater/Traits/TraitSparklyWater.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Cwl.LangMod;
+
+namespace Sparkly.Traits;
+
+internal class TraitSparklyWater : TraitDrink
+{
+    private static readonly HashSet<CardRow> _cached = [];
+
+    internal static string SparklyName = "sparkly water";
+    
+    public override EffectId IdEffect => EffectId.Booze;
+    public override bool IsNeg => false;
+
+    public override void OnDrink(Chara c)
+    {
+        base.OnDrink(c);
+        c.Say($"sparkly_love_{rnd(9)}", c);
+    }
+
+    internal static void TransformBooze(ref string traitName, Card traitOwner)
+    {
+        if (traitName != nameof(TraitDrink)) {
+            return;
+        }
+        
+        var row = traitOwner.sourceCard;
+        if (!row.trait.Contains(nameof(EffectId.Booze))) {
+            return;
+        }
+
+        // mutate trait
+        traitName = nameof(TraitSparklyWater);
+        
+        // mutate row
+        if (!_cached.Add(row)) {
+            return;
+        }
+        
+        row.tag = row.tag
+            .Where(t => t != nameof(CTAG.neg))
+            .ToArray();
+        row.name = "sparkly_fmt".Loc(row.name, SparklyName);
+    }
+}
