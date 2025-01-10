dbb319edc1555a6788531c49a37ab82c6f3b47b8
DK <35783662+gottyduke@users.noreply.github.com>
2025-01-09 18:34:50 -0600

    chore(CWL): move `Patches` folder (too large)

---
+++ CustomWhateverLoader/Loader/CwlDebug.cs
@@ -0,0 +1,33 @@
+﻿#if !DEBUG
+global using SwallowExceptions.Fody;
+#endif
+
+using System;
+using Cwl.ThirdParty;
+using UnityEngine;
+
+namespace Cwl;
+
+#if DEBUG
+internal class SwallowExceptions : Attribute;
+#endif
+
+internal partial class CwlMod
+{
+    private void SetupExceptionHook()
+    {
+        Glance.TryConnect();
+
+        Application.logMessageReceived += ExceptionHandler;
+    }
+
+    private static void ExceptionHandler(string message, string stackTrace, LogType type)
+    {
+        if (type is not (LogType.Error or LogType.Exception)) {
+            return;
+        }
+
+        if (message is null or "" || stackTrace is null or "") {
+        }
+    }
+}
