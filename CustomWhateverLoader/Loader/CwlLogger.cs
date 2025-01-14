52015018d2a7848ab1d5b0ff22130a002f235488
DK <35783662+gottyduke@users.noreply.github.com>
2025-01-13 22:51:56 -0600

    chore(CWL): inline logger

--- CustomWhateverLoader/Loader/CwlLogger.cs
+++ CustomWhateverLoader/Loader/CwlLogger.cs
@@ -5,16 +5,19 @@ namespace Cwl;
 
 internal sealed partial class CwlMod
 {
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     internal static void Log(object payload)
     {
         UnityEngine.Debug.Log($"[CWL][INFO] {payload}");
     }
 
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     internal static void Log<T>(object payload)
     {
         Log($"[{typeof(T).Name}] {payload}");
     }
 
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     internal static void Debug(object payload, [CallerMemberName] string caller = "")
     {
         if (!CwlConfig.Logging.Verbose?.Value is true) {
@@ -24,6 +27,7 @@ internal sealed partial class CwlMod
         UnityEngine.Debug.Log($"[CWL][DEBUG] [{caller}] {payload}");
     }
 
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     internal static void Debug<T>(object payload, [CallerMemberName] string caller = "")
     {
         if (!CwlConfig.Logging.Verbose?.Value is true) {
@@ -33,23 +37,27 @@ internal sealed partial class CwlMod
         Debug($"[{typeof(T).Name}] {payload}", caller);
     }
 
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     internal static void Warn(object payload)
     {
         UnityEngine.Debug.Log($"[CWL][WARN] {payload}");
         Glance.Dispatch(payload);
     }
 
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     internal static void Warn<T>(object payload)
     {
         Warn($"[{typeof(T).Name}] {payload}");
     }
 
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     internal static void Error(object payload, [CallerMemberName] string caller = "")
     {
         UnityEngine.Debug.Log($"[CWL][ERROR] [{caller}] {payload}");
         Glance.Dispatch(payload);
     }
 
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     internal static void Error<T>(object payload, [CallerMemberName] string caller = "")
     {
         Error($"[{typeof(T).Name}] {payload}", caller);
