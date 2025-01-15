13e7e67dc188f50511a0e4f0042b8270bc73ea01
DK <35783662+gottyduke@users.noreply.github.com>
2025-01-15 02:40:19 -0600

    fix(CWL): workaround for Material rows indexing

---
+++ CustomWhateverLoader/Patches/Objs/ReverseIdMapper.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using HarmonyLib;
+
+namespace Cwl.Patches.Objs;
+
+[HarmonyPatch]
+internal class ReverseIdMapper
+{
+    internal static IEnumerable<MethodInfo> TargetMethods()
+    {
+        return [
+            AccessTools.Method(typeof(ThingGen), nameof(ThingGen.CreateObj)),
+            AccessTools.Method(typeof(Map), nameof(Map.SetObj),
+                [typeof(int), typeof(int), typeof(int), typeof(int), typeof(int)]),
+        ];
+    }
+
+    [HarmonyPrefix]
+    internal static void OnSetObjId(ref int id)
+    {
+        var objs = EMono.sources.objs;
+        id = objs.rows.IndexOf(objs.map[id]);
+    }
+}
