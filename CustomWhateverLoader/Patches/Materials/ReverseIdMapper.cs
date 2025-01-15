92009a1b3ae4fadf3b0411d284275acc81341ad3
DK <35783662+gottyduke@users.noreply.github.com>
2025-01-15 02:40:19 -0600

    fix(CWL): workaround for Material rows indexing

---
+++ CustomWhateverLoader/Patches/Materials/ReverseIdMapper.cs
@@ -0,0 +1,29 @@
+﻿using HarmonyLib;
+
+namespace Cwl.Patches.Materials;
+
+[HarmonyPatch]
+internal class ReverseIdMapper
+{
+    [SwallowExceptions]
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(Card), nameof(Card.c_dyeMat), MethodType.Getter)]
+    internal static void OnGetIdMat(Card __instance, ref int __result)
+    {
+        var mat = EMono.sources.materials;
+        __result = mat.rows.IndexOf(mat.map[__result]);
+    }
+
+    [SwallowExceptions]
+    [HarmonyPrefix]
+    [HarmonyPatch(typeof(Card), nameof(Card.Create))]
+    internal static void OnSetIdMat(ref int _idMat)
+    {
+        if (_idMat == -1) {
+            return;
+        }
+
+        var mat = EMono.sources.materials;
+        _idMat = mat.rows.IndexOf(mat.map[_idMat]);
+    }
+}
