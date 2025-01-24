727c6415ed3e296bef684f7fc0db61baba031d5b
DK <35783662+gottyduke@users.noreply.github.com>
2025-01-24 02:04:28 -0600

    feat(CWL): prevent supply quests with invalid mod items from crashing

---
+++ CustomWhateverLoader/Patches/Quests/InvalidateSupplyPatch.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using Cwl.Helper.Runtime;
+using HarmonyLib;
+
+namespace Cwl.Patches.Quests;
+
+[HarmonyPatch]
+internal class InvalidateSupplyPatch
+{
+    internal static IEnumerable<MethodInfo> TargetMethods()
+    {
+        return OverrideMethodComparer.FindAllOverrides(typeof(QuestDeliver), nameof(QuestDeliver.ListDestThing), typeof(bool));
+    }
+
+    [HarmonyPrefix]
+    internal static bool InvalidateItems(QuestDeliver __instance, ref List<Thing> __result)
+    {
+        if (EMono.sources.things.map.ContainsKey(__instance.idThing)) {
+            return true;
+        }
+
+        __result = [];
+        CwlMod.Warn<InvalidateSupplyPatch>($"quest {__instance.GetType().Name} has invalid item id: {__instance.idThing}\n" +
+                                           $"CWL caught the exception and kept the game going");
+        
+        return false;
+    }
+}
