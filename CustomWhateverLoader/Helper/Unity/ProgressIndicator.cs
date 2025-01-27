37912f295f63860154681e7ec2b405eab369e633
DK <35783662+gottyduke@users.noreply.github.com>
2025-01-27 11:15:04 -0600

    feat(CWL): refactor `ProgressIndicator` to interval based update

--- CustomWhateverLoader/Helper/Unity/ProgressIndicator.cs
+++ CustomWhateverLoader/Helper/Unity/ProgressIndicator.cs
@@ -2,38 +2,64 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using DG.Tweening;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 namespace Cwl.Helper.Unity;
 
-public class ProgressIndicator : EMono
+[RequireComponent(typeof(PopItemText))]
+public class ProgressIndicator : EMono, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler
 {
     private static readonly List<ProgressIndicator> _active = [];
 
+    private bool _hoverable;
+    private Func<string>? _onHover;
+    private Outline? _outline;
     private ProgressUpdater? _updater;
 
     public PopItemText Pop => GetComponent<PopItemText>();
+    public bool Expanded { get; private set; }
 
-    private void Update()
+    private void Start()
     {
-        if (_updater is null) {
-            Destroy(this);
-            return;
-        }
+        StartCoroutine(Sync(0.2f));
+    }
 
-        var (onUpdate, shouldKill, linger) = _updater;
-        if (Pop == null) {
-            return;
-        }
-
-        Sync(onUpdate());
-
-        if (shouldKill()) {
-            StartCoroutine(DeferredKill(linger, onUpdate));
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (eventData.button == PointerEventData.InputButton.Right) {
+            Kill();
         }
     }
 
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (!_hoverable) {
+            return;
+        }
+
+        Expanded = true;
+        _outline!.enabled = true;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (!_hoverable) {
+            return;
+        }
+
+        Expanded = false;
+        _outline!.enabled = false;
+    }
+
+    /// <summary>
+    ///     a wrapper of <see cref="PopItemText" /> that offers continuous updating and various controls
+    /// </summary>
+    /// <param name="onUpdate">update func</param>
+    /// <param name="shouldKill">predicate to notify kill</param>
+    /// <param name="lingerDuration">default 10 seconds linger duration</param>
+    /// <returns>the created instance, null if failed</returns>
     [SwallowExceptions]
     public static ProgressIndicator? CreateProgress(Func<UpdateInfo> onUpdate, Func<bool> shouldKill, float lingerDuration = 10f)
     {
@@ -47,50 +73,94 @@ public class ProgressIndicator : EMono
         }
 
         ui.popSystem.insert = false;
+        ui.popSystem.maxLines = int.MaxValue;
 
         pop.name = "PopProgress";
         pop.important = true;
         pop.text.alignment = TextAnchor.UpperLeft;
         pop.Rect().pivot = new(1f, 1f);
 
-        var progress = pop.gameObject.GetOrAddComponent<ProgressIndicator>();
-        progress._updater = new(onUpdate, shouldKill, lingerDuration);
+        var progress = pop.GetOrCreate<ProgressIndicator>();
+        progress._updater = new(onUpdate, shouldKill, Mathf.Max(lingerDuration, 0f));
 
         _active.Add(progress);
         return progress;
     }
 
+    /// <summary>
+    ///     extremely simple progress that kills itself on scope exit
+    /// </summary>
+    /// <param name="onUpdate">update func</param>
+    /// <param name="lingerDuration">default 10 seconds linger duration</param>
+    /// <returns>
+    ///     <see cref="IDisposable" /> resource via using statement, underlying <see cref="ProgressIndicator" /> can
+    ///     be accessed through property <see cref="ScopeExit.Object" />
+    /// </returns>
     public static ScopeExit CreateProgressScoped(Func<UpdateInfo> onUpdate, float lingerDuration = 10f)
     {
-        var scopedExit = new ScopeExit();
-        CreateProgress(onUpdate, () => !scopedExit.Alive, lingerDuration);
-        return scopedExit;
+        var scopeExit = new ScopeExit();
+        scopeExit.Object = CreateProgress(onUpdate, () => !scopeExit.Alive, lingerDuration);
+        return scopeExit;
     }
 
-    private IEnumerator DeferredKill(float linger, Func<UpdateInfo> onUpdate)
+    public ProgressIndicator SetHover(Func<string>? onHover = null)
     {
-        Sync(onUpdate());
+        if (_hoverable) {
+            return this;
+        }
 
-        yield return new WaitForSecondsRealtime(linger);
+        _hoverable = true;
+        _onHover = onHover;
 
-        Pop.important = false;
+        _outline = GetComponentInChildren<Image>().GetOrCreate<Outline>();
+        _outline.effectDistance = new(2f, 2f);
+        _outline.effectColor = Color.black;
+        _outline.enabled = false;
 
-        _updater = null;
-        _active.Remove(this);
-        ui.popSystem.Kill(Pop);
+        this.GetOrCreate<UICollider>();
+
+        return this;
     }
 
-    private void Sync(UpdateInfo info)
+    private IEnumerator Sync(float interval)
     {
-        Pop.SetText(info.Text, info.Sprite, info.Color ?? default(Color));
+        if (_updater is null) {
+            yield break;
+        }
+
+        var (onUpdate, shouldKill, lingerTime) = _updater;
+
+        while (!shouldKill()) {
+            UpdatePopup(onUpdate);
+            yield return new WaitForSeconds(interval);
+        }
+
+        while (lingerTime > 0f) {
+            UpdatePopup(onUpdate);
+            yield return new WaitForSeconds(interval);
+            lingerTime -= interval;
+        }
+
+        Kill();
+    }
+
+    private void UpdatePopup(Func<UpdateInfo> onUpdate)
+    {
+        var info = onUpdate();
+        var text = info.Text;
+
+        if (_hoverable && _onHover is not null) {
+            var append = Expanded ? _onHover() : "<Hover For More Information>";
+            text += $"\b{append}";
+        }
+
+        Pop.SetText(text, info.Sprite, info.Color ?? default(Color));
         AdjustPosition();
     }
 
     private void AdjustPosition()
     {
         var rect = Pop.Rect();
-        rect.DOComplete();
-
         var y = 0f;
         foreach (var progress in _active.OfType<ProgressIndicator>()) {
             if (progress == this) {
@@ -100,6 +170,13 @@ public class ProgressIndicator : EMono
 
             y -= progress.Pop.Rect().sizeDelta.y;
         }
+    }
+
+    private void Kill()
+    {
+        Pop.important = false;
+        _active.Remove(this);
+        ui.popSystem.Kill(Pop);
     }
 
     public record UpdateInfo(string Text, Sprite? Sprite = null, Color? Color = null);
---
+++ CustomWhateverLoader/Patches/SkipProgressTweenPatch.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
+using Cwl.Helper.Extensions;
+using Cwl.Helper.Unity;
+using HarmonyLib;
+
+namespace Cwl.Patches;
+
+[HarmonyPatch]
+internal class SkipProgressTweenPatch
+{
+    internal static IEnumerable<MethodInfo> TargetMethods()
+    {
+        return [
+            AccessTools.Method(typeof(PopManager), nameof(PopManager.Pop)),
+            AccessTools.Method(typeof(PopManager), "_Kill"),
+        ];
+    }
+
+    [HarmonyTranspiler]
+    internal static IEnumerable<CodeInstruction> OnPopTweenIl(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+    {
+        return new CodeMatcher(instructions, generator)
+            .End()
+            .MatchStartBackwards(
+                new OpCodeContains(nameof(OpCodes.Ldloc)),
+                new(OpCodes.Ldc_I4_1),
+                new(OpCodes.Add),
+                new OpCodeContains(nameof(OpCodes.Stloc)))
+            .ThrowIfInvalid("failed to match1")
+            .CreateLabel(out var @continue)
+            .MatchEndBackwards(
+                new(OpCodes.Ldarg_0),
+                new OperandContains(OpCodes.Ldfld, nameof(PopManager.items)),
+                new OpCodeContains(nameof(OpCodes.Ldloc)),
+                new OperandContains(OpCodes.Callvirt, "Item"),
+                new OperandContains(OpCodes.Call, nameof(ClassExtension.Rect)))
+            .ThrowIfInvalid("failed to match2")
+            .CreateLabel(out var proceed)
+            .InsertAndAdvance(
+                new(OpCodes.Dup),
+                Transpilers.EmitDelegate(ShouldSkipTween),
+                new(OpCodes.Brfalse, proceed),
+                new(OpCodes.Pop),
+                new(OpCodes.Br, @continue))
+            .InstructionEnumeration();
+    }
+
+    private static bool ShouldSkipTween(PopItem item)
+    {
+        return item.GetComponent<ProgressIndicator>() != null;
+    }
+}
