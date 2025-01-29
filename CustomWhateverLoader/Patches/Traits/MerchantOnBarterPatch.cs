29ccb7e64574afc6e0421c4dfdb9570a6b1f07a3
DK <35783662+gottyduke@users.noreply.github.com>
2025-01-28 23:16:59 -0600

    feat(CWL): use pre-post patch for custom stock in case other mods don't execute original

--- CustomWhateverLoader/Patches/Traits/MerchantOnBarterPatch.cs
+++ CustomWhateverLoader/Patches/Traits/MerchantOnBarterPatch.cs
@@ -1,32 +1,22 @@
-﻿using System.Collections.Generic;
-using System.Reflection.Emit;
-using Cwl.API.Custom;
+﻿using Cwl.API.Custom;
 using HarmonyLib;
 
 namespace Cwl.Patches.Traits;
 
-[HarmonyPatch]
+[HarmonyPatch(typeof(Trait), nameof(Trait.OnBarter))]
 internal class MerchantOnBarterPatch
 {
-    [HarmonyTranspiler]
-    [HarmonyPatch(typeof(Trait), nameof(Trait.OnBarter))]
-    internal static IEnumerable<CodeInstruction> OnRestockIl(IEnumerable<CodeInstruction> instructions)
+    [HarmonyPrefix]
+    internal static void OnSetStock(Trait __instance, out bool __state)
     {
-        return new CodeMatcher(instructions)
-            .MatchEndForward(
-                new(OpCodes.Ldarg_0),
-                new(OpCodes.Callvirt, AccessTools.PropertyGetter(
-                    typeof(Trait),
-                    nameof(Trait.ShopType))))
-            .InsertAndAdvance(
-                new(OpCodes.Dup),
-                Transpilers.EmitDelegate(ShouldGenerate))
-            .InstructionEnumeration();
+        __state = __instance is CustomMerchant merchant &&
+                  EClass.world.date.IsExpired(merchant.owner.c_dateStockExpire);
     }
 
-    private static void ShouldGenerate(Trait trait)
+    [HarmonyPostfix]
+    private static void ShouldGenerate(Trait __instance, bool __state)
     {
-        if (trait is CustomMerchant merchant) {
+        if (__instance is CustomMerchant merchant && __state) {
             merchant.Generate();
         }
     }
