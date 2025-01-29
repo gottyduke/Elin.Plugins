d43336507654af6eecf63dc678684757ee395580
DK <35783662+gottyduke@users.noreply.github.com>
2025-01-28 23:17:47 -0600

    feat(CWL): support new `forceRarity` tag for items using category with `fixedRarity`

---
+++ CustomWhateverLoader/Patches/Recipes/ForceRarityPatch.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using Cwl.Helper.Runtime;
+using HarmonyLib;
+
+namespace Cwl.Patches.Recipes;
+
+[HarmonyPatch]
+internal class ForceRarityPatch
+{
+    internal static IEnumerable<MethodInfo> TargetMethods()
+    {
+        return OverrideMethodComparer.FindAllOverrides(typeof(Card), nameof(Card.OnCreate), typeof(int));
+    }
+
+    [SwallowExceptions]
+    [HarmonyPrefix]
+    internal static void OnResetRarity(Card __instance)
+    {
+        var row = __instance.sourceCard;
+        if (row.tag.Contains("forceRarity")) {
+            __instance.ChangeRarity(row.quality.ToEnum<Rarity>());
+        }
+    }
+}
