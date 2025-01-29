77b726f6f0ac540f227f9763094192d74c29c2be
DK <35783662+gottyduke@users.noreply.github.com>
2025-01-29 02:26:15 -0600

    feat(CWL): support new `fixedElement` `randomElement` tags for element source value

---
+++ CustomWhateverLoader/Patches/Elements/FixedSourceValuePatch.cs
@@ -0,0 +1,18 @@
+﻿using HarmonyLib;
+
+namespace Cwl.Patches.Elements;
+
+[HarmonyPatch]
+internal class FixedSourceValuePatch
+{
+    [HarmonyPrefix]
+    [HarmonyPatch(typeof(ElementContainer), nameof(ElementContainer.ApplyElementMap))]
+    internal static void OnSetFixedSourceValue(ElementContainerCard __instance, ref SourceValueType type)
+    {
+        if (__instance.owner.sourceCard.tag.Contains("fixedElement")) {
+            type = SourceValueType.Fixed;
+        } else if (__instance.owner.sourceCard.tag.Contains("randomElement")) {
+            type = SourceValueType.EquipmentRandom;
+        }
+    }
+}
