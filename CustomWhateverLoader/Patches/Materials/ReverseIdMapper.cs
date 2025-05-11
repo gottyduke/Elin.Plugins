39e927881fdf5aa2e20d21b4fc8f0146d2b7ee13
DK <35783662+gottyduke@users.noreply.github.com>
2025-05-11 16:30:36 -0500

    fix: safeguard default `-1` index

--- CustomWhateverLoader/Patches/Materials/ReverseIdMapper.cs
+++ CustomWhateverLoader/Patches/Materials/ReverseIdMapper.cs
@@ -29,6 +29,9 @@ internal class ReverseIdMapper
     {
         var mat = EMono.sources.materials;
         var id = mat.rows.IndexOf(mat.map.GetValueOrDefault(idMat));
+        if (id == -1) {
+            id = 0;
+        }
         idMat = id;
     }
 
@@ -64,10 +67,10 @@ internal class ReverseIdMapper
             return new CodeMatcher(instructions)
                 .MatchEndForward(
                     new OperandMatch(OpCodes.Callvirt, o => o.ToString().Contains("List<SourceMaterial+Row>::get_Item")))
-                .Repeat(cm => {
-                    cm.SetInstructionAndAdvance(
-                        Transpilers.EmitDelegate(ReverseIndexer));
-                })
+                .Repeat(cm => cm
+                    .SetInstructionAndAdvance(
+                        Transpilers.EmitDelegate(ReverseIndexer))
+                )
                 .InstructionEnumeration();
         }
     }
