83b3abaa436a92f68ef5392ef408ebf050062200
DK <35783662+gottyduke@users.noreply.github.com>
2025-06-11 22:03:06 -0500

    feat(CWL): support for `<sound=ID,[chance]>` tag in chara text

---
+++ CustomWhateverLoader/Patches/Sounds/AudibleTopicTextPatch.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Reflection.Emit;
+using System.Text.RegularExpressions;
+using Cwl.Helper.Extensions;
+using HarmonyLib;
+
+namespace Cwl.Patches.Sounds;
+
+[HarmonyPatch]
+internal class AudibleTopicTextPatch
+{
+    [HarmonyTranspiler]
+    [HarmonyPatch(typeof(Chara), nameof(Chara.GetTopicText))]
+    internal static IEnumerable<CodeInstruction> OnTopicTextIl(IEnumerable<CodeInstruction> instructions)
+    {
+        return new CodeMatcher(instructions)
+            .End()
+            .MatchEndBackwards(
+                new OperandContains(OpCodes.Call, nameof(ClassExtension.RandomItem)),
+                new(OpCodes.Ret))
+            .InsertAndAdvance(
+                new(OpCodes.Ldarg_0),
+                Transpilers.EmitDelegate(TryPlaySoundText))
+            .InstructionEnumeration();
+    }
+
+    private static string TryPlaySoundText(string text, Chara chara)
+    {
+        var match = Regex.Match(text, "<sound=([^>]+)>");
+
+        if (!match.Success || match.Groups.Count <= 1) {
+            return text;
+        }
+
+        var soundExpr = match.Groups[1].Value.Split(',');
+        var soundId = soundExpr[0];
+
+        if (soundExpr.TryGet(1, true) is not { } chance ||
+            EClass.rndf(1f) <= chance.AsFloat(1f)) {
+            chara.PlaySound(soundId);
+        }
+
+        return text.Remove(match.Index, match.Length);
+    }
+}
