6cf2438809822786a698e4db3a7c33e18de7f3df
DK <35783662+gottyduke@users.noreply.github.com>
2025-07-26 08:56:32 -0500

    feat(CWL): load ref dice for element

---
+++ CustomWhateverLoader/Patches/Calcs/LoadRefDicePatch.cs
@@ -0,0 +1,27 @@
+﻿using Cwl.API.Custom;
+using HarmonyLib;
+
+namespace Cwl.Patches.Calcs;
+
+[HarmonyPatch]
+internal class LoadRefDicePatch
+{
+    [HarmonyPrefix]
+    [HarmonyPatch(typeof(Dice), nameof(Dice.Create), typeof(string), typeof(int), typeof(Card), typeof(Act))]
+    internal static void OnReplaceNullDice(ref string id, Act? act)
+    {
+        if (act?.source is not { } source) {
+            return;
+        }
+
+        if (!source.tag.Contains("addDice") &&
+            // enable ref dice for all custom elements
+            !CustomElement.Managed.ContainsKey(source.id)) {
+            return;
+        }
+
+        if (EMono.sources.calc.map.ContainsKey(source.alias)) {
+            id = source.alias;
+        }
+    }
+}
