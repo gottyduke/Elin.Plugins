c21894629783be095903a629f56dfce917a247a6
DK <35783662+gottyduke@users.noreply.github.com>
2025-07-29 11:35:44 -0500

    feat(CMM): add more config

--- CharacterCustomizerPlus/CmmConfig.cs
+++ CharacterCustomizerPlus/CmmConfig.cs
@@ -7,6 +7,9 @@ internal class CmmConfig
     internal static ConfigEntry<int> MaxPartsPerRow { get; private set; } = null!;
     internal static ConfigEntry<int> PartCellWidth { get; private set; } = null!;
     internal static ConfigEntry<int> PartCellHeight { get; private set; } = null!;
+    internal static ConfigEntry<bool> EnableSliderIcon { get; private set; } = null!;
+    internal static ConfigEntry<float> SliderIconScale { get; private set; } = null!;
+    internal static ConfigEntry<float> RotateButtonScale { get; private set; } = null!;
 
     internal static void Load(ConfigFile config)
     {
@@ -14,25 +17,50 @@ internal class CmmConfig
             ModInfo.Name,
             "MaxPartsPerRow",
             4,
-            "Maximum allowed parts displayed on the same row\n" +
-            "同一行允许显示的最大部件数");
+            "Maximum allowed part previews displayed on the same row\n" +
+            "同一行允许显示的最大部件预览数");
 
         PartCellWidth = config.Bind(
             ModInfo.Name,
             "PartCellWidth",
             128,
             new ConfigDescription(
-                "Width of the part cell in the parts picker window\n" +
-                "部件按钮的宽度",
-                new AcceptableValueRange<int>(64, 128)));
+                "Width of the part preview in the parts picker window\n" +
+                "部件预览图的宽度",
+                new AcceptableValueRange<int>(64, 192)));
 
         PartCellHeight = config.Bind(
             ModInfo.Name,
             "PartCellHeight",
             192,
             new ConfigDescription(
-                "Height of the part cell in the parts picker window\n" +
-                "部件按钮的长度",
-                new AcceptableValueRange<int>(96, 192)));
+                "Height of the part preview in the parts picker window\n" +
+                "部件预览图的长度",
+                new AcceptableValueRange<int>(96, 288)));
+        
+        EnableSliderIcon = config.Bind(
+            ModInfo.Name,
+            "EnableSliderIcon",
+            true,
+            "Replace the slider buttons with custom textures\n" +
+            "替换两个滑条方块的贴图");
+        
+        SliderIconScale = config.Bind(
+            ModInfo.Name,
+            "SliderIconScale",
+            1f,
+            new ConfigDescription(
+                "Scaling ratio of custom slider textures\n" +
+                "滑条自定义贴图的大小缩放",
+                new AcceptableValueRange<float>(0.5f, 2f)));
+
+        RotateButtonScale = config.Bind(
+            ModInfo.Name,
+            "RotateButtonScale",
+            2f,
+            new ConfigDescription(
+                "Scaling ratio of rotate buttons\n" +
+                "旋转按钮的大小缩放",
+                new AcceptableValueRange<float>(1f, 5f)));
     }
 }
--- CharacterCustomizerPlus/Patches/PccWindowAddon.cs
+++ CharacterCustomizerPlus/Patches/PccWindowAddon.cs
@@ -19,24 +19,34 @@ internal class PccWindowAddon
                 return;
             }
 
-            var deltaBar = content.GetFirstChildWithName("Slider delta");
-            if (deltaBar != null) {
-                deltaBar.GetComponent<Slider>().image.sprite = "cmm_delta".LoadSprite();
-            }
-
-            var scaleBar = content.GetFirstChildWithName("Slider");
-            if (scaleBar != null) {
-                scaleBar.GetComponent<Slider>().image.sprite = "cmm_scale".LoadSprite();
-            }
-
+            var rotateScaler = CmmConfig.RotateButtonScale.Value;
             var rotateLeft = content.GetFirstChildWithName("UIButton left");
             if (rotateLeft != null) {
-                rotateLeft.Rect().sizeDelta *= 2f;
+                rotateLeft.Rect().sizeDelta *= rotateScaler;
             }
 
             var rotateRight = content.GetFirstChildWithName("UIButton right");
             if (rotateRight != null) {
-                rotateRight.Rect().sizeDelta *= 2f;
+                rotateRight.Rect().sizeDelta *= rotateScaler;
+            }
+
+            if (!CmmConfig.EnableSliderIcon.Value) {
+                return;
+            }
+
+            var sliderScaler = CmmConfig.SliderIconScale.Value;
+            var deltaBar = content.GetFirstChildWithName("Slider delta");
+            if (deltaBar != null) {
+                var image = deltaBar.GetComponent<Slider>().image;
+                image.sprite = "cmm_delta".LoadSprite();
+                image.rectTransform.sizeDelta *= sliderScaler;
+            }
+
+            var scaleBar = content.GetFirstChildWithName("Slider");
+            if (scaleBar != null) {
+                var image = scaleBar.GetComponent<Slider>().image;
+                image.sprite = "cmm_scale".LoadSprite();
+                image.rectTransform.sizeDelta *= sliderScaler;
             }
         } catch (Exception ex) {
             CmmMod.Log($"failed to patch edit pcc layer {ex}");
