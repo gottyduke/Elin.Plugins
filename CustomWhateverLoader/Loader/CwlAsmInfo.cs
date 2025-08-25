112d939f6525c0d226e250496f0eb0bad1a81d7f
gottyduke <35783662+gottyduke@users.noreply.github.com>
2025-08-25 12:09:40 -0500

    build(CWL): compiled for 23.188 Stable

--- CustomWhateverLoader/Loader/CwlAsmInfo.cs
+++ CustomWhateverLoader/Loader/CwlAsmInfo.cs
@@ -4,6 +4,6 @@ using Cwl;
 [assembly: AssemblyVersion(ModInfo.Version)]
 [assembly: AssemblyFileVersion(ModInfo.Version)]
 [assembly: AssemblyInformationalVersion(GitVersionInformation.CommitDate)]
-[assembly: AssemblyProduct(ModInfo.Name)]
+[assembly: AssemblyProduct(GitVersionInformation.Sha)]
 [assembly: AssemblyTitle(ModInfo.Name)]
 [assembly: AssemblyMetadata("Repo", "https://github.com/gottyduke/Elin.Plugins/tree/master/CustomWhateverLoader")]
