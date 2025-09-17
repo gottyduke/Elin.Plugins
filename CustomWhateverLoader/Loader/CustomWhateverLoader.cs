f45da069f9ef9d04c22c789a788f2f3f89030a42
gottyduke <35783662+gottyduke@users.noreply.github.com>
2025-09-17 03:11:03 -0500

    chore(CWL): bump version

--- CustomWhateverLoader/Loader/CustomWhateverLoader.cs
+++ CustomWhateverLoader/Loader/CustomWhateverLoader.cs
@@ -16,7 +16,7 @@ public static class ModInfo
 #endif
     public const string Guid = "dk.elinplugins.customdialogloader";
     public const string Name = "Custom Whatever Loader";
-    public const string Version = "1.20.46";
+    public const string Version = "1.20.47";
 }
 
 [BepInPlugin(ModInfo.Guid, ModInfo.Name, ModInfo.Version)]
--- CustomWhateverLoader/Loader/CwlAsmInfo.cs
+++ CustomWhateverLoader/Loader/CwlAsmInfo.cs
@@ -5,5 +5,5 @@ using Cwl;
 [assembly: AssemblyFileVersion(ModInfo.Version)]
 [assembly: AssemblyInformationalVersion($"{GitVersionInformation.CommitDate}+{GitVersionInformation.Sha}")]
 [assembly: AssemblyProduct($"{ModInfo.Guid}+{GitVersionInformation.BranchName}")]
-[assembly: AssemblyTitle(ModInfo.Name)]
+[assembly: AssemblyTitle($"{ModInfo.Name}+{ModInfo.TargetVersion}")]
 [assembly: AssemblyMetadata("Repo", "https://github.com/gottyduke/Elin.Plugins/tree/master/CustomWhateverLoader")]
