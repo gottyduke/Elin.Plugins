0b7bf6e40ad83a02d5480d35de115fee000bc31a
gottyduke <35783662+gottyduke@users.noreply.github.com>
2025-09-21 17:09:44 -0500

    build(CWL): bump version

--- CustomWhateverLoader/Loader/CustomWhateverLoader.cs
+++ CustomWhateverLoader/Loader/CustomWhateverLoader.cs
@@ -17,7 +17,7 @@ public static class ModInfo
 #endif
     public const string Guid = "dk.elinplugins.customdialogloader";
     public const string Name = "Custom Whatever Loader";
-    public const string Version = "1.20.47";
+    public const string Version = "1.20.48";
 }
 
 [BepInPlugin(ModInfo.Guid, ModInfo.Name, ModInfo.Version)]
