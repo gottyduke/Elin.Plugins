a14b0d46e894ddc8f708ed344c5b60ca1b2996f4
gottyduke <35783662+gottyduke@users.noreply.github.com>
2025-09-26 17:06:02 -0500

    fix(CWL): support custom religion sprite in journal

--- CustomWhateverLoader/Patches/Religions/SetReligionPortraitPatch.cs
+++ CustomWhateverLoader/Patches/Religions/SetReligionPortraitPatch.cs
@@ -1,7 +1,6 @@
 ﻿using Cwl.API.Custom;
 using Cwl.Helper.Unity;
 using HarmonyLib;
-using MethodTimer;
 using UnityEngine;
 
 namespace Cwl.Patches.Religions;
@@ -11,7 +10,6 @@ internal class SetReligionPortraitPatch
 {
     private static readonly int _mainTex = Shader.PropertyToID("_MainTex");
 
-    [Time]
     [SwallowExceptions]
     [HarmonyPostfix]
     [HarmonyPatch(typeof(LayerDrama), nameof(LayerDrama.Activate))]
@@ -25,14 +23,23 @@ internal class SetReligionPortraitPatch
             return;
         }
 
-        if (!SpriteReplacer.dictModItems.TryGetValue(custom.id, out var file)) {
+        var portrait = LayerDrama.Instance.drama.dialog.portrait?.portrait;
+        var sprite = custom.GetSprite();
+
+        portrait?.sprite = sprite;
+        portrait?.material.SetTexture(_mainTex, sprite?.texture);
+    }
+
+    [SwallowExceptions]
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(Religion), nameof(Religion.GetSprite))]
+    internal static void OnSetReligionSprite(Religion __instance, ref Sprite __result)
+    {
+        var overrideSprite = __instance.id.LoadSprite(resizeWidth: 150, resizeHeight: 200);
+        if (overrideSprite == null) {
             return;
         }
 
-        var portrait = LayerDrama.Instance.drama.dialog.portrait?.portrait;
-        var sprite = file.LoadSprite() ?? portrait?.sprite;
-
-        portrait?.sprite = sprite;
-        portrait?.material?.SetTexture(_mainTex, sprite?.texture);
+        __result = overrideSprite;
     }
 }
