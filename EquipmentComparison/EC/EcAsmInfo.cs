0abf15035e16fda59fcfddc296b69a8b8981154c
gottyduke <35783662+gottyduke@users.noreply.github.com>
2025-10-01 00:05:34 -0500

    build(EC): cleanup project files

---
+++ EquipmentComparison/EC/EcAsmInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+using EC;
+
+[assembly: AssemblyVersion($"{ModInfo.Version}.*")]
+[assembly: AssemblyFileVersion(ModInfo.Version)]
+[assembly: AssemblyInformationalVersion($"{GitVersionInformation.CommitDate}+{GitVersionInformation.Sha}")]
+[assembly: AssemblyProduct($"{ModInfo.Guid}+{GitVersionInformation.BranchName}")]
+[assembly: AssemblyTitle(ModInfo.Name)]
+[assembly: AssemblyMetadata("Repo", "https://github.com/gottyduke/Elin.Plugins/tree/master/EquipmentComparison")]
