82ce4981b5ce40cfe45726b1877430ed47e76710
gottyduke <35783662+gottyduke@users.noreply.github.com>
2025-10-03 15:47:47 -0500

    feat(CWL): safe get race row from chara row

---
+++ CustomWhateverLoader/Patches/Races/SafeGetRaceRowPatch.cs
@@ -0,0 +1,19 @@
+using HarmonyLib;
+
+namespace Cwl.Patches.Races;
+
+[HarmonyPatch]
+internal class SafeGetRaceRowPatch
+{
+    [HarmonyPrefix]
+    [HarmonyPatch(typeof(SourceChara.Row), nameof(SourceChara.Row.race_row), MethodType.Getter)]
+    internal static void OnSafeGetRaceRow(SourceChara.Row __instance)
+    {
+        if (EClass.sources.races.map.ContainsKey(__instance.race)) {
+            return;
+        }
+
+        __instance.race = "norland";
+        CwlMod.Warn<SourceRace>($"replaced invalid race {__instance.race} on {__instance.id}");
+    }
+}
