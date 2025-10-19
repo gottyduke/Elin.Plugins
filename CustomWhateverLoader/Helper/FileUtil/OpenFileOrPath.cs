51640db5c1c6b117dfb4fcd16bd35130363bfa37
gottyduke <35783662+gottyduke@users.noreply.github.com>
2025-10-19 02:37:05 -0500

    fix(CWL): OpenFileOrPath

--- CustomWhateverLoader/Helper/FileUtil/OpenFileOrPath.cs
+++ CustomWhateverLoader/Helper/FileUtil/OpenFileOrPath.cs
@@ -1,45 +1,70 @@
-using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using Cwl.Helper.String;
-using Cwl.LangMod;
 
 namespace Cwl.Helper.FileUtil;
 
 public class OpenFileOrPath
 {
+    private static readonly List<Process> _unclosed = [];
+
     public static void Run(string path)
     {
+        if (!File.Exists(path)) {
+            return;
+        }
+
         path = path.NormalizePath();
+
         try {
-            Process.Start(path);
+            _unclosed.Add(Process.Start(path));
+            return;
         } catch {
-            CwlMod.Popup<OpenFileOrPath>("cwl_ui_failed_shellex".Loc());
+            // noexcept
+        }
 
-            var proton = !"PROTON_VERSION".EnvVar.IsEmpty() ||
-                         !"STEAM_COMPAT_DATA_PATH".EnvVar.IsEmpty();
+        // open with notepad
+        var proton = !"PROTON_VERSION".EnvVar.IsEmpty() ||
+                     !"STEAM_COMPAT_DATA_PATH".EnvVar.IsEmpty();
+        try {
+            if (proton) {
+                Process.Start("xdg-open", $"\"{path}\"");
+            } else {
+                _unclosed.Add(Process.Start("notepad.exe", path));
+            }
+
+            return;
+        } catch {
+            // noexcept
+        }
+
+        // open folder
+        try {
+            path = Path.GetDirectoryName(path)!.NormalizePath();
+
+            if (!Directory.Exists(path)) {
+                return;
+            }
+
+            if (proton) {
+                Process.Start("xdg-open", $"\"{path}\"");
+            } else {
+                Process.Start("explorer.exe", path);
+            }
+        } catch {
+            // noexcept
+        }
+    }
+
+    public static void ForceCloseAllReferences()
+    {
+        foreach (var process in _unclosed) {
             try {
-                if (proton) {
-                    Process.Start("xdg-open", $"\"{path}\"");
-                } else {
-                    Process.Start("notepad.exe", path);
-                }
-            } catch (Exception ex) {
-                CwlMod.Popup<OpenFileOrPath>("cwl_ui_failed_shellex".Loc(path, ex.Message));
-
-                try {
-                    path = Path.GetDirectoryName(path)!.NormalizePath();
-                    if (proton) {
-                        Process.Start("xdg-open", $"\"{path}\"");
-                    } else {
-                        Process.Start("explorer.exe", path);
-                    }
-                } catch {
-                    // noexcept
-                }
+                process.Kill();
+            } catch {
                 // noexcept
             }
-            // noexcept
         }
     }
 }
