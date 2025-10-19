b2c93e4c225c57304265c7f38a95b3a91dc1df21
gottyduke <35783662+gottyduke@users.noreply.github.com>
2025-10-19 02:36:55 -0500

    feat(CWL): add file watcher helper

---
+++ CustomWhateverLoader/Helper/FileUtil/FileWatcherHelper.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.CompilerServices;
+using Cwl.LangMod;
+
+namespace Cwl.Helper.FileUtil;
+
+public class FileWatcherHelper
+{
+    private static readonly Dictionary<string, (FileSystemWatcher, Action<FileSystemEventArgs>)> _watchers = [];
+
+    public static void Register(string id,
+                                string path,
+                                string filter,
+                                Action<FileSystemEventArgs> handler,
+                                [CallerMemberName] string caller = "")
+    {
+        if (_watchers.ContainsKey(id)) {
+            return;
+        }
+
+        var watcher = new FileSystemWatcher(path, filter) {
+            IncludeSubdirectories = true,
+            NotifyFilter = NotifyFilters.FileName
+                           | NotifyFilters.LastWrite
+                           | NotifyFilters.CreationTime
+                           | NotifyFilters.Size,
+            EnableRaisingEvents = true,
+        };
+
+        watcher.Created += SafeNotify;
+        watcher.Deleted += SafeNotify;
+        watcher.Changed += SafeNotify;
+        watcher.Renamed += SafeNotify;
+
+        _watchers[id] = (watcher, handler);
+
+        CwlMod.Log<FileWatcherHelper>("cwl_log_processor_add".Loc("file_watch", $"{id}:{filter}", caller));
+
+        return;
+
+        void SafeNotify(object _, FileSystemEventArgs args)
+        {
+            try {
+                handler(args);
+            } catch (Exception ex) {
+                CwlMod.Warn<FileWatcherHelper>("cwl_warn_processor".Loc("file_watch", $"{id}:{filter}", ex));
+            }
+        }
+    }
+
+    public static void Unregister(string id)
+    {
+        if (!_watchers.Remove(id, out var watcher)) {
+            return;
+        }
+
+        watcher.Item1.EnableRaisingEvents = false;
+        watcher.Item1.Dispose();
+    }
+}
