dbe60e778e1db155dfbf0f67fb176f1b31d5b801
gottyduke <35783662+gottyduke@users.noreply.github.com>
2025-12-10 06:44:17 -0600

    ci(ACS): add build ci

---
+++ AnimatedCustomSprites/AcsAsmInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+using ACS;
+
+[assembly: AssemblyVersion($"{ModInfo.Version}.*")]
+[assembly: AssemblyFileVersion($"{ModInfo.Version}.{GitVersionInformation.CommitsSinceVersionSource}")]
+[assembly: AssemblyInformationalVersion($"{GitVersionInformation.CommitDate}+{GitVersionInformation.Sha}")]
+[assembly: AssemblyProduct($"{ModInfo.Guid}+{GitVersionInformation.BranchName}")]
+[assembly: AssemblyTitle(ModInfo.Name)]
+[assembly: AssemblyMetadata("Repo", "https://github.com/gottyduke/Elin.Plugins/tree/master/AnimatedCustomSprites")]
