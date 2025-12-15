d4079b44b56e6c20150e3dc1c1517840ed63ebf8
gottyduke <35783662+gottyduke@users.noreply.github.com>
2025-12-15 16:38:35 -0600

    feat(CWL): allow reloading all mod textures

--- CustomWhateverLoader/Loader/DataLoaders/TransformLoader.cs
+++ CustomWhateverLoader/Loader/DataLoaders/TransformLoader.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.IO;
+using Cwl.Helper.FileUtil;
+using Cwl.Helper.String;
 using Cwl.Helper.Unity;
+using ReflexCLI.Attributes;
 using Object = UnityEngine.Object;
 
 namespace Cwl;
@@ -24,5 +28,18 @@ internal partial class DataLoader
         CwlMod.Debug<DataLoader>($"Relocated sprite {texId} > {sprite.name}");
 
         return true;
+    }
+
+    [ConsoleCommand("load_sprites")]
+    internal static void RefreshAllPackageTextures()
+    {
+        var textures = PackageIterator.GetRelocatedDirsFromPackage("Texture");
+        foreach (var textureDir in textures) {
+            foreach (var texture in textureDir.GetFiles("*.png", SearchOption.AllDirectories)) {
+                var container = Path.GetRelativePath(textureDir.FullName, texture.FullName).NormalizePath();
+                var id = Path.ChangeExtension(container, null).NormalizePath();
+                SpriteReplacer.dictModItems[id] = Path.ChangeExtension(texture.FullName, null).NormalizePath();
+            }
+        }
     }
 }
