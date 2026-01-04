b71bd68b825413cda508c4b3e5e73172f605d390
gottyduke <35783662+gottyduke@users.noreply.github.com>
2026-01-04 06:50:50 -0600

    fix(MV): use version check to disable dragging after 23.253

--- ModViewerPlus/Patches/LayerModAddon.cs
+++ ModViewerPlus/Patches/LayerModAddon.cs
@@ -12,15 +12,20 @@ internal class LayerModAddon
     [HarmonyPatch(typeof(LayerMod), nameof(LayerMod.OnInit))]
     internal static void OnLayerModInit(LayerMod __instance)
     {
+        __instance.windows[0].AddBottomButton("mvm_ui_save", ModListManager.SaveModList);
+        __instance.windows[0].AddBottomButton("mvm_ui_load", ModListManager.LoadModList);
+        __instance.windows[0].AddBottomButton("mvm_ui_remove", ModListManager.RemoveModList);
+
+        // EA 23.253 integrated mod viewer
+        if (!Core.Instance.version.IsBelow(0, 23, 253)) {
+            return;
+        }
+
         var vlg = __instance.list._layoutItems;
         vlg.GetOrCreate<DragController>();
 
         foreach (Transform mod in vlg.transform) {
             mod.GetOrCreate<DragBehaviour>();
         }
-
-        __instance.windows[0].AddBottomButton("mvm_ui_save", ModListManager.SaveModList);
-        __instance.windows[0].AddBottomButton("mvm_ui_load", ModListManager.LoadModList);
-        __instance.windows[0].AddBottomButton("mvm_ui_remove", ModListManager.RemoveModList);
     }
 }
