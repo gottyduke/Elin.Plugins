eff4eeccfc0b2696c1464b19ce8ee465e9e71616
Redgeioz <redgeiozw@gmail.com>
2026-02-20 12:27:10 +0800

    feat(EMP): estimated client action progress bar

---
+++ ElinTogether/ElinTogether/Patches/GaugeUpdateValuePatch.cs
@@ -0,0 +1,15 @@
+using HarmonyLib;
+
+namespace ElinTogether.Patches;
+
+[HarmonyPatch(typeof(Gauge), nameof(Gauge.UpdateValue), [typeof(float), typeof(float)])]
+internal class GaugeUpdateValuePatch
+{
+    [HarmonyPrefix]
+    internal static void OnGaugeUpdateValue(ref float now)
+    {
+        if (now < 0 && EClass.pc.ai.GetProgress() is { } progress) {
+            now = progress.CurrentProgress + int.MaxValue;
+        }
+    }
+}
