9b5499c36c0ed4fec5d7a9059d7a468220ce9006
gottyduke <35783662+gottyduke@users.noreply.github.com>
2026-04-24 23:21:55 -0500

    fix(CWL): purge missing mod zones upon init

--- CustomWhateverLoader/Loader/CustomWhateverLoader.cs
+++ CustomWhateverLoader/Loader/CustomWhateverLoader.cs
@@ -22,7 +22,7 @@ public static class ModInfo
     public const string Guid = "dk.elinplugins.customdialogloader";
     public const string Name = "Custom Whatever Loader";
 
-    public const string Version = "1.22.18";
+    public const string Version = "1.22.20";
 
     // for runtime versions
     public static string BuildVersion => field ??= CwlMod.Assembly.GetName().Version.ToString();
---
+++ CustomWhateverLoader/Patches/Zones/SafeMapInitPatch.cs
@@ -0,0 +1,23 @@
+using Cwl.LangMod;
+using HarmonyLib;
+
+namespace Cwl.Patches.Zones;
+
+[HarmonyPatch]
+internal class SafeMapInitPatch
+{
+    [HarmonyPrefix]
+    [HarmonyPatch(typeof(EloMap), nameof(EloMap.Init))]
+    internal static void OnInitCells(EloMap __instance)
+    {
+        __instance.region.children.ForeachReverse(z => {
+            if (EMono.sources.zones.map.ContainsKey(z.id)) {
+                return;
+            }
+
+            __instance.region.children.Remove(z);
+            CwlMod.Log<EloMap>("cwl_warn_deserialize".Loc(nameof(Spatial), z.id, $"{z.x}/{z.y}",
+                CwlConfig.Patches.SafeCreateClass.Definition.Key));
+        });
+    }
+}
