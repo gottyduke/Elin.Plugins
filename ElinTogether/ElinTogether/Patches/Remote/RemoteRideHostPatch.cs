eacfa5686fbf8c92d81c7fc91e4d67ed5a0bccac
Redgeioz <redgeiozw@gmail.com>
2026-05-07 20:46:54 +0800

    fix(EMP): character rendering when riding a remote player

--- ElinTogether/ElinTogether/Patches/Remote/RemoteRideHostPatch.cs
+++ ElinTogether/ElinTogether/Patches/Remote/RemoteRideHostPatch.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Reflection.Emit;
 using HarmonyLib;
 
 namespace ElinTogether.Patches;
@@ -12,5 +14,21 @@ internal class RemoteRideHostPatch
         if (EClass.pc.host is { } host) {
             EClass.player.position = host.renderer.position;
         }
+    }
+
+    [HarmonyTranspiler]
+    [HarmonyPatch(typeof(BaseTileMap), nameof(BaseTileMap.DrawTile))]
+    internal static IEnumerable<CodeInstruction> OnDrawTileIl(IEnumerable<CodeInstruction> instructions)
+    {
+        // if (!chara.IsPC && !chara.renderer.IsMoving && this.detail.charas.Count > 1 && (this.detail.charas.Count != 2 || !this.detail.charas[0].IsDeadOrSleeping || !this.detail.charas[0].IsPCC))
+        // {
+        //     this._actorPos += this.renderSetting.charaPos[1 + ((num29 < 4) ? num29 : 3)];
+        // }
+        return new CodeMatcher(instructions)
+            .MatchStartForward(
+                new CodeMatch(OpCodes.Callvirt, AccessTools.PropertyGetter(typeof(Card), nameof(Chara.IsPC))))
+            .SetInstruction(
+                Transpilers.EmitDelegate((Chara chara) => chara.IsPC || EClass.pc.host == chara))
+            .InstructionEnumeration();
     }
 }
