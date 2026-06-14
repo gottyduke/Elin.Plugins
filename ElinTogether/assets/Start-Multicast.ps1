99b3e217a07a049ac370014ec219f0f5c5fe272d
gottyduke <35783662+gottyduke@users.noreply.github.com>
2026-06-13 20:48:23 -0500

    chore(EMP): update launch script

--- ElinTogether/assets/Start-Multicast.ps1
+++ ElinTogether/assets/Start-Multicast.ps1
@@ -1,11 +1,11 @@
+$steam = "$($env:SteamPath)\steam.exe"
 
+& $steam "steam://rungameid/2135150"
 
-$instance1 = "$($env:ElinGamePath)\Elin.exe"
-& $instance1
+$startExe = Join-Path $env:SandboxPath "Start.exe"
 
-$instance2 = "$($env:SandboxPath)\Start.exe"
-& $instance2 /box:D1 $instance1
+& $startExe /box:D1 $steam "steam://rungameid/2135150"
 
-if ($args[0] -eq $True) {
-    & $instance2 /box:D2 $instance1
+if ($args[0]) {
+    & $startExe /box:D2 $steam "steam://rungameid/2135150"
 }
