536d258a45edca4908e259e596fc3d45adcbef3e
gottyduke <35783662+gottyduke@users.noreply.github.com>
2026-07-08 03:25:41 -0500

    ci(KoC): add GitVersion.MSBuild nuget deps

--- KarmaOnCaught/KarmaOnCaught/Koc/AsmInfo.cs
+++ KarmaOnCaught/KarmaOnCaught/Koc/AsmInfo.cs
@@ -2,5 +2,8 @@ using System.Reflection;
 using KarmaOnCaught;
 
 [assembly: AssemblyVersion($"{ModInfo.Version}.*")]
-[assembly: AssemblyFileVersion(ModInfo.Version)]
+[assembly: AssemblyFileVersion($"{ModInfo.Version}.{GitVersionInformation.CommitsSinceVersionSource}")]
+[assembly: AssemblyInformationalVersion($"{GitVersionInformation.CommitDate}+{GitVersionInformation.Sha}")]
+[assembly: AssemblyProduct($"{ModInfo.Guid}+{GitVersionInformation.BranchName}")]
 [assembly: AssemblyTitle(ModInfo.Name)]
+[assembly: AssemblyMetadata("Repo", "https://github.com/gottyduke/Elin.Plugins/tree/master/KarmaOnCaught")]
